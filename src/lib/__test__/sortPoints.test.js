3624e23a20e9d6dbaa4b1769ba2eae1ecdd0d23b
Taijen Ave-Lallemant <batman@10-249-20-178.wireless.oregonstate.edu>
2023-06-08 14:11:54 -0700

    Refactored existing tests, implemented a new test for empty inputs

--- src/lib/__test__/sortPoints.test.js
+++ src/lib/__test__/sortPoints.test.js
@@ -2,18 +2,24 @@
 const sortPoints = require('../sortPoints');
 
 // Write unit tests for sortPoints
-describe('sortPoints', () => {
-  test('sortPoints1', () => {
-    const points = [{x: 10, y: 5}, {x: 6, y: 6}]
-    const sortedPoints = sortPoints(points)
-    expect(sortedPoints).toEqual([{x: 6, y: 6},{x: 10, y: 5}])
+describe('sortPoints handles valid cases', () => {
+  // We will put a bunch of valid cases in here
+  test('sortPoints returns [{x: 6, y:6}, {x: 10, y: 5}, {x: 362, y: 2}] on input of [{x: 10, y: 5}, {x: 362, y: 2}, {x: 6, y: 6}]', () =>{
+    const points = [{x: 10, y: 5}, {x: 362, y: 2}, {x: 6, y: 6}];
+    const sortedPoints = sortPoints(points);
+    expect(sortedPoints).toEqual([{x:6, y: 6}, {x: 10, y: 5}, {x: 362, y: 2}]);
   });
 
-  test('should sort an array of points in descending order', () => {
-    const points = [{x: 1, y: 2}, {x: 3, y: 4}]
+  test('sortPoints returns [{x: 1, y: 2}, {x: 3, y: 4}, {x: 7, y: 1}] on input of [{x: 3, y: 4}, {x: 7, y: 1}, {x: 1, y: 2}]', () => {
+    const points = [{x: 3, y: 4}, {x: 7, y: 1}, {x: 1, y: 2}];
     const sortedPoints = sortPoints(points, 'desc');
-    expect(sortedPoints).toEqual([{x: 1, y: 2}, {x: 3, y: 4}]);
+    expect(sortedPoints).toEqual([{x: 1, y: 2}, {x: 3, y: 4}, {x: 7, y: 1}]);
   });
 
-  // Add more test cases to cover different scenarios
+  test('sortPoints returns [] on input of []', () =>{
+    const points = [];
+    const sortedPoints = sortPoints(points);
+    expect(sortedPoints).toEqual(points);
+  });
 });
+
