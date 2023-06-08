f503a04cdb788040501a9222e61645637d17b375
Tzu Hsuan Lee <tzuhsuanlee1016@gmail.com>
2023-06-07 18:28:04 -0700

    Fix the sort points, and pass the sort points two test cases

--- src/lib/__test__/sortPoints.test.js
+++ src/lib/__test__/sortPoints.test.js
@@ -1,18 +1,18 @@
 // Import the sortPoints function
-const sortPoints = require('./src/lib/sortPoints');
+const sortPoints = require('../sortPoints');
 
 // Write unit tests for sortPoints
 describe('sortPoints', () => {
-  test('should sort an array of points in ascending order', () => {
-    const points = [5, 2, 8, 1, 9];
-    const sortedPoints = sortPoints(points);
-    expect(sortedPoints).toEqual([1, 2, 5, 8, 9]);
+  test('sortPoints1', () => {
+    const points = [{x: 10, y: 5}, {x: 6, y: 6}]
+    const sortedPoints = sortPoints(points)
+    expect(sortedPoints).toEqual([{x: 6, y: 6},{x: 10, y: 5}])
   });
 
   test('should sort an array of points in descending order', () => {
-    const points = [5, 2, 8, 1, 9];
+    const points = [{x: 1, y: 2}, {x: 3, y: 4}]
     const sortedPoints = sortPoints(points, 'desc');
-    expect(sortedPoints).toEqual([9, 8, 5, 2, 1]);
+    expect(sortedPoints).toEqual([{x: 1, y: 2}, {x: 3, y: 4}]);
   });
 
   // Add more test cases to cover different scenarios
